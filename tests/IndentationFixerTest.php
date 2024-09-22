70b4ca77cac75030cd698c124eb08b3683c9a279
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-09-22 20:51:59 +0700

    Create IndentationFixerTest.php

---
+++ tests/IndentationFixerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use phpcodestyle\fixers\IndentationFixer;
+
+class IndentationFixerTest extends TestCase
+{
+    public function testFix()
+    {
+        $fixer = new IndentationFixer();
+        $content = "    function test() {}\n    function anotherTest() {}";
+        $expected = "\tfunction test() {}\n\tfunction anotherTest() {}";
+
+        $this->assertEquals($expected, $fixer->fix($content));
+    }
+}
