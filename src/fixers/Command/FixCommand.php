6a8fbfb13e40a7bac53fde683c4712e0a655b611
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-09-22 21:10:19 +0700

    Update FixCommand.php

--- src/fixers/Command/FixCommand.php
+++ src/fixers/Command/FixCommand.php
@@ -6,7 +6,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use phpcodestyle\fixers\FixerManager;
+use phpcodestyle\FixerManager;
 
 class FixCommand extends Command
 {
