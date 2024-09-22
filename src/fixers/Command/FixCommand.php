74fc38aaff85facac19729e94f9e5a68d7b40837
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-09-22 20:40:04 +0700

    Create FixCommand.php

---
+++ src/fixers/Command/FixCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace phpcodestyle\fixers\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use phpcodestyle\fixers\FixerManager;
+
+class FixCommand extends Command
+{
+    protected static $defaultName = 'fix';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Fix PHP code style.')
+            ->addArgument('path', InputArgument::REQUIRED, 'The path to the PHP files.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $path = $input->getArgument('path');
+        $manager = new FixerManager();
+        $manager->fix($path);
+        
+        $output->writeln('Code style fixed.');
+        return Command::SUCCESS;
+    }
+}
